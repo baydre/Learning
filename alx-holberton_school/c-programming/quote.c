d8a5b7d988d4784bd567053bf79fa4f66a14bc8c
Yasir Musa <baydre2@gmail.com>
2023-03-19 23:28:26 +0100

    Added C basics file task

---
+++ alx-holberton_school/c-programming/quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints another bunch of strings.
+ *
+ * Return: One '1' Failure.
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korar, 2015-10-19\n");
+
+	return (1);
+}
---
+++ alx-holberton_school/c-programming/size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the size of various type on the computer.
+ *
+ * Return: Zero '0' success
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+}
